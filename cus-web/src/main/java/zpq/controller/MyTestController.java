6f603e038f1d891518d0ee1b74794aaa13f3d943
zcp5935 <2251745121@qq.com>
2019-09-30 11:40:19 +0800

    controller add order

--- cus-web/src/main/java/zpq/controller/MyTestController.java
+++ cus-web/src/main/java/zpq/controller/MyTestController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import zpq.bean.Customer;
 import zpq.bean.CustomerExample;
+import zpq.bean.MyOrder;
+import zpq.bean.MyOrderExample;
 import zpq.dao.CustomerMapper;
+import zpq.dao.MyOrderMapper;
 
 @Controller
 public class MyTestController {
@@ -17,9 +20,18 @@ public class MyTestController {
 	@Autowired
 	private CustomerMapper customerMapper;
 
+	@Autowired
+	private MyOrderMapper myOrderMapper;
+
 	@RequestMapping("qryAllCustomers")
 	@ResponseBody
-	public List<Customer> qryCustomer() {
+	public List<Customer> qryAllCustomers() {
 		return customerMapper.selectByExample(new CustomerExample());
+	}
+
+	@RequestMapping("qryAllOrders")
+	@ResponseBody
+	public List<MyOrder> qryAllOrders() {
+		return myOrderMapper.selectByExample(new MyOrderExample());
 	}
 }
