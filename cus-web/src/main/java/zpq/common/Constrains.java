662ba8c39ba5b05bb045f9792ce8c8e13bec0307
zhangchaopei <zhangchaopei@csii.com.cn>
2020-04-17 13:07:26 +0800

    00

--- cus-web/src/main/java/zpq/common/Constrains.java
+++ cus-web/src/main/java/zpq/common/Constrains.java
@@ -3,13 +3,19 @@ package zpq.common;
 public interface Constrains {
 
 	String ADD = "add";
+
 	
+
 	String SUBTRACT = "subtract";
-	
+
 	String TH = "TH";
+
 	
 	String TH3 = "TH3";
 	
 	String TH4 = "TH4";
+
+
+
 
 }
